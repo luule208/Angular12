ca4984edf432cac6def8192b877a325b80a7b2fb
luule <ryuu2897@gmail.com>
2022-05-29 21:19:52 +0700

    update code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public name = 'Lưu Lê';
+  public age = 25;
   constructor() { }
 
   ngOnInit(): void {
