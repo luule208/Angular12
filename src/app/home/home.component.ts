9a7c0c029d2664e3eed7d035313cb42e544b6b1a
luule <ryuu2897@gmail.com>
2022-06-04 05:40:15 +0700

    update code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,14 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name = 'Lưu Lê';
   public age = 25;
-  public traicay = ['Táo','Nho', 'Cam', 'Quit','Ổi'];
+  public traicay = ['Táo', 'Nho', 'Cam', 'Quit', 'Ổi'];
+  public traicay2 = [
+    { ten: 'Táo', gia: 12, hagia: true },
+    { ten: 'Nho', gia: -1.241, hagia: false },
+    { ten: 'Cam', gia: 8, hagia: true },
+  ];
   constructor() { }
 
   ngOnInit(): void {
-    console.log('trai cay = ' ,this.traicay);
+    console.log('trai cay = ', this.traicay);
   }
 
-  public changeAge():void{
+  public changeAge(): void {
     console.log(this.age);
   }
 
