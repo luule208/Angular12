cb4a164483460692a83226d7e3b70d43d620905c
luule <ryuu2897@gmail.com>
2022-06-15 01:50:18 +0700

    update code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,6 +14,29 @@ export class HomeComponent implements OnInit {
     { ten: 'Nho', gia: -1.241, hagia: false },
     { ten: 'Cam', gia: 8, hagia: true },
   ];
+  public cities = [
+    {
+      city: 'Chọn thành phố',
+      district: ['Quận huyện']
+    },
+    {
+      city: 'An Giang',
+      district: [
+        'Thành phố Long Xuyên',
+        'Thành phố Châu Đốc',
+        'Huyện An Phú'
+      ]
+    },
+    {
+      city: 'Bà Rịa - Vũng Tàu',
+      district: [
+        'Thành phố Vũng Tàu',
+        'Thị xã Bà Rịa',
+        'Huyện Côn Đảo'
+      ]
+    },
+  ]
+  public district = ['Quận huyện'];
   constructor() { }
 
   ngOnInit(): void {
@@ -22,6 +45,23 @@ export class HomeComponent implements OnInit {
 
   public changeAge(): void {
     console.log(this.age);
+  }
+
+  public changeCity(event: any) {
+    console.log('event', event.target.value);
+    const city = event.target.value;
+    if (!city) {
+      return;
+    }
+
+    // TODO: cách 1
+    // const search = this.cities.filter(data => data.city === city);
+    // if (search && search.length > 0) {
+    //   this.district = search[0].district;
+    // }
+
+    // TODO: cách 2
+    this.district = this.cities.find(data => data.city === city)?.district || [];
   }
 
 }
