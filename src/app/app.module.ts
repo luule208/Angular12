19c6d2654c865b410bc40b5a80841841d49ef05c
Luu Le <ryuu208@gmail.com>
2022-06-30 02:09:29 +0700

    update code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { HighlightDirective } from './Directive/highlight.directive';
 import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { GetDataComponent } from './get-data/get-data.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,8 +24,15 @@ import { ReactiveFormsModule } from '@angular/forms';
     HighlightDirective,
     TemplateDrivenFormComponent,
     ReactiveFormComponent,
+    GetDataComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
